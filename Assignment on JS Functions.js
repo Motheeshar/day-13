96528f8e234c99b3ba11558e2964d350f2299fbe
A R Motheesh <ar.motheesh@gmail.com>
2024-02-19 19:43:29 +0530

    Add files via upload

---
+++ Assignment on JS Functions.js
@@ -0,0 +1,31 @@
+// Challenge 1
+function ageCalculator(birthYear, presentYear) {
+    let age = presentYear - birthYear;
+    console.log("Age:", age);
+    return age;
+  }
+  
+  ageCalculator(2000, 2024);
+  
+  // Challenge 2
+  function addition(a, b) {
+    let sum = a + b;
+    console.log(sum);
+    return sum;
+  }
+  
+  addition(2, 3);
+  
+  // Challenge 3
+  console.log(x);
+  var x = "I can access it.";
+  console.log(x);
+  
+  // Challenge 4
+  const addNumbers = (a, b) => {
+    let sum = a + b;
+    console.log("Sum:", sum);
+    return sum;
+  };
+  
+  addNumbers(5, 7);
